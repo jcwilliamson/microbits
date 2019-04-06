2a8a3c21f512f7411ab30b6486de9a558c9a7b07
jcwilliamson <47338736+jcwilliamson@users.noreply.github.com>
2019-04-06 22:02:46 +0100

    Add files via upload

---
+++ outreach_code/transmit.js
@@ -0,0 +1,28 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (1 == receivedNumber) {
+        images.createImage(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `).showImage(0)
+    } else {
+        images.createImage(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `).showImage(0)
+    }
+})
+radio.setGroup(1)
+radio.setTransmitPower(7)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        radio.sendNumber(1)
+    } else {
+        radio.sendNumber(0)
+    }
+})
